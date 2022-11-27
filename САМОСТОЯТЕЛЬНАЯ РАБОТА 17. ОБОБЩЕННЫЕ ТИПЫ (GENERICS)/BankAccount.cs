9de3743a9270554afb9607f892b6c94a1fb422d8
Tolmachev DY <Tolmachev DY@DESKTOP-5RJHKAS>
2022-11-27 19:13:42 +0300

    Добавьте файлы проекта.

---
+++ САМОСТОЯТЕЛЬНАЯ РАБОТА 17. ОБОБЩЕННЫЕ ТИПЫ (GENERICS)/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace САМОСТОЯТЕЛЬНАЯ_РАБОТА_17.ОБОБЩЕННЫЕ_ТИПЫ__GENERICS_
+{
+    /*Создать класс для моделирования счета в банке. Предусмотреть закрытые поля для номера счета, баланса, ФИО владельца.  Класс должен быть объявлен как обобщенный. 
+    Универсальный параметр T должен определять тип номера счета. Разработать  методы  для  доступа  к  данным  –  заполнения  и  чтения.*/
+    class BankAccount<T>//создаем обобщенный класс
+    {
+        T Score { get; set; }//создаем параметр с универсальным типом данных
+        double Balance { get; set; }
+        string FullName { get; set; }
+
+        public void Input()//создаем метод для заполнения данных
+        {
+            Console.WriteLine("Введите номер счета");
+            Score = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));//приводим введенное значение к универсальному типу данных
+            Console.WriteLine("Введите баланс");
+            Balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            FullName = Console.ReadLine();
+        }
+        public string GetInfo()//создаем метод для чтения данных
+        {
+            return $"{FullName} н/с:{Score} баланс:{Balance:f2} ";
+        }
+    }
+}
---
+++ САМОСТОЯТЕЛЬНАЯ РАБОТА 17. ОБОБЩЕННЫЕ ТИПЫ (GENERICS)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace САМОСТОЯТЕЛЬНАЯ_РАБОТА_17.ОБОБЩЕННЫЕ_ТИПЫ__GENERICS_
+{
+    class Program
+    {
+        /*Создать  несколько экземпляров класса, параметризированного различными типам. 
+        Заполнить его поля и вывести на экран информацию об экземпляре класса.*/
+        static void Main(string[] args)
+        {
+            BankAccount<int> bankAccount1 = new BankAccount<int>();//создаем экземпляр класса, параметрезированного типом данных int 
+            bankAccount1.Input();//на экземпляре класса вызываем метод заполнения данных 
+            Console.WriteLine(bankAccount1.GetInfo());//выводим на консоль данные, считанные методом из класса
+            BankAccount<string> bankAccount2 = new BankAccount<string>();//создаем экземпляр класса, параметрезированного типом данных string 
+            bankAccount2.Input();//на экземпляре класса вызываем метод заполнения данных
+            Console.WriteLine(bankAccount2.GetInfo());//выводим на консоль данные, считанные методом из класса
+            Console.ReadKey();
+        }
+    }
+}
